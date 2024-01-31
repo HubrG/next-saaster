828066107d999291b1c29f4827578fb74cb69094
HubrG <hubrgiorgi@gmail.com>
2024-01-31 23:40:26 +0100

    Features finished, Update code formatting and UI components

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,8 +1,9 @@
 import { Navbar } from "@/src/components/layout/header/Navbar";
 import createMetadata from "@/src/lib/metadatas";
-import { NextIntlClientProvider, useLocale, useMessages } from "next-intl";
+import { NextIntlClientProvider, useMessages } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 import "react-toastify/dist/ReactToastify.css";
+import { locales } from "../../src/lib/intl/navigation";
 
 export const generateMetadata = async () => {
   return createMetadata({
@@ -17,20 +18,17 @@ type Props = {
     locale: string;
   };
 };
-const locales = ["en", "fr"];
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
+
 export default function LocaleLayout(props: Props) {
   const {
     children,
     params: { locale },
   } = props;
   unstable_setRequestLocale(locale);
-
-  // Show a 404 page if the locale is not supported
-  const loc = useLocale();
 
   const messages = useMessages();
   //

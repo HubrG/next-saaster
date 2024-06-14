c55aafea51a14824478a30b355a7f16823a79bd2
HubrG <hubrgiorgi@gmail.com>
2024-06-14 17:42:25 +0200

    Blog

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,11 @@
 // is requested that doesn't match the middleware and
 // therefore doesn't have a locale associated with it.
 
-import { Link } from "@/src/lib/intl/navigation";
 import createMetadata from "@/src/lib/metadatas";
 import { Session } from "next-auth";
+import { getLocale, getTranslations } from "next-intl/server";
+import Link from "next/link";
+import LocaleLayout from "./[locale]/layout";
 export const generateMetadata = async () => {
   return createMetadata({
     // Voir la configuration des métadonnées dans metadatas.ts
@@ -26,13 +28,15 @@ const Sess: Session = {
 };
 
 export default async function NotFound() {
-  // const t = await getTranslations();
-  // const locale = await getLocale();
+  const t = await getTranslations();
+  const locale = await getLocale();
   return (
-    <div className=" text-center flex items-center justify-center flex-col max-w-lg !-mt-96">
-      <h2>Erreur 404</h2>
-      <p>blabla</p>
-      <Link href="/">Blalba</Link>
-    </div>
+    <LocaleLayout params={{ locale }} session={Sess}>
+      <div className=" text-center flex items-center justify-center flex-col max-w-lg !-mt-96">
+        <h2>{t("NotFound.erreur-404")}</h2>
+        <p>{t("NotFound.page-not-found")}</p>
+        <Link href="/" locale={locale}>{t("NotFound.back-link")}</Link>
+      </div>
+    </LocaleLayout>
   );
 }

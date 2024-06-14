63555a0e7f181d13aa69cabf8e6b803f1de0ff7f
HubrG <hubrgiorgi@gmail.com>
2024-06-14 17:36:01 +0200

    Blog

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,8 +5,6 @@
 import { Link } from "@/src/lib/intl/navigation";
 import createMetadata from "@/src/lib/metadatas";
 import { Session } from "next-auth";
-import { getLocale, getTranslations } from "next-intl/server";
-import LocaleLayout from "./[locale]/layout";
 export const generateMetadata = async () => {
   return createMetadata({
     // Voir la configuration des métadonnées dans metadatas.ts
@@ -28,15 +26,13 @@ const Sess: Session = {
 };
 
 export default async function NotFound() {
-  const t = await getTranslations();
-  const locale = await getLocale();
+  // const t = await getTranslations();
+  // const locale = await getLocale();
   return (
-    <LocaleLayout params={{ locale }} session={Sess}>
-      <div className=" text-center flex items-center justify-center flex-col max-w-lg !-mt-96">
-        <h2>Erreur 404</h2>
-        <p>blabla</p>
-        <Link href="/">Blalba</Link>
-      </div>
-    </LocaleLayout>
+    <div className=" text-center flex items-center justify-center flex-col max-w-lg !-mt-96">
+      <h2>Erreur 404</h2>
+      <p>blabla</p>
+      <Link href="/">Blalba</Link>
+    </div>
   );
 }

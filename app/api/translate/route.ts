5bbbd63dd788d0e590aa01dd2d282f3756fb2a11
HubrG <hubrgiorgi@gmail.com>
2024-06-20 01:34:17 +0200

    Notification & languages

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -5,6 +5,12 @@ export async function POST(req: NextRequest) {
   const apiKey = process.env.TRANSLATE_API_KEY || "";
   const url = "https://api-free.deepl.com/v2/translate";
 
+  // Entourer les mots à ne pas traduire avec des balises <keep>
+  const textWithKeepTags = text.replace(
+    /(NextJS|Boilerplate|Next|Next-Auth|Resend|Stripe|SEO|API)/g,
+    "<keep>$1</keep>"
+  );
+
   try {
     const response = await fetch(url, {
       method: "POST",
@@ -13,24 +19,41 @@ export async function POST(req: NextRequest) {
       },
       body: new URLSearchParams({
         auth_key: apiKey,
-        text: text,
+        text: textWithKeepTags,
         target_lang: targetLang,
         source_lang: defaultLocale,
+        tag_handling: "xml", // Indiquer que nous utilisons des balises XML
+        ignore_tags: "keep", // Indiquer à l'API de ne pas traduire le contenu des balises <keep>
       }),
     });
 
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      const errorText = await response.text();
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${errorText}`
+      );
     }
 
     const textResponse = await response.text();
     const data = textResponse ? JSON.parse(textResponse) : {};
 
+    // Retirer les balises <keep> du texte traduit
+    if (
+      data.translations &&
+      data.translations[0] &&
+      data.translations[0].text
+    ) {
+      data.translations[0].text = data.translations[0].text.replace(
+        /<\/?keep>/g,
+        ""
+      );
+    }
+
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     console.error("Error translating with DeepL:", error);
     return NextResponse.json(
-      { error: "Error translating with DeepL" },
+      { error: `Error translating with DeepL` },
       { status: 500 }
     );
   }
